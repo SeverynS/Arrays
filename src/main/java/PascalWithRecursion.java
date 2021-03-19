1fdc46928ff1e3f54ed921b74de760df66be2b90
SeverynZ <s.zlochovsky@gmail.com>
2021-03-19 16:30:24 +0200

    Some optimization

--- src/main/java/PascalWithRecursion.java
+++ src/main/java/PascalWithRecursion.java
@@ -8,7 +8,6 @@ public class PascalWithRecursion {
 
         int[][] recursionTriangle = new int[10][];
         secondLevelArr(recursionTriangle, 0);
-        externalSide(recursionTriangle, 0);
         internalSide(recursionTriangle, 0, 0);
 
         for (int tri[] :  recursionTriangle) {
@@ -17,50 +16,28 @@ public class PascalWithRecursion {
     }
 
     private static void secondLevelArr(int[][] triangle, int i){
-        int temp = i;
 
-        if(temp < 10) {
-            triangle[temp] = new int[temp + 1];
-            temp++;
-            secondLevelArr(triangle, temp);
-        }
-    }
-
-    private static void externalSide(int[][] triangle, int i) {
-        int temp = i;
-
-        if(temp < 10) {
-            triangle[temp][0] = 1;
-            triangle[temp][triangle[temp].length - 1] = 1;
-
-            if(temp >= 2) {
-                triangle[temp][1] = temp;
-            }
-
-            if(temp >= 3) {
-                triangle[temp][triangle[temp].length - 2] = temp;
-            }
-
-            temp++;
-            externalSide(triangle, temp);
+        if(i < 10) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][triangle[i].length - 1] = 1;
+            i++;
+            secondLevelArr(triangle, i);
         }
     }
 
     private static void internalSide(int[][] triangle, int i, int j) {
 
-        int tempi = i;
-        int tempj = j;
-
-        if(tempi < 10) {
-            while (tempj < triangle[tempi].length - 1) {
-                if (triangle[tempi][tempj] == 0) {
-                    triangle[tempi][tempj] = triangle[tempi - 1][tempj - 1] + triangle[tempi - 1][tempj];
+        if(i < 10) {
+            while (j < triangle[i].length - 1) {
+                if (triangle[i][j] == 0) {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
                 }
-                tempj++;
+                j++;
             }
-            tempi++;
-            tempj = 1;
-            internalSide(triangle, tempi, tempj);
+            i++;
+            j = 1;
+            internalSide(triangle, i, j);
         }
     }
 }
