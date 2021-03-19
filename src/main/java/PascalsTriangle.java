b0b4a539e36de1109cda84c6f4970537d2674dd4
SeverynZ <s.zlochovsky@gmail.com>
2021-03-19 17:58:09 +0200

    Some optimization

--- src/main/java/PascalsTriangle.java
+++ src/main/java/PascalsTriangle.java
@@ -13,27 +13,19 @@ public class PascalsTriangle {
     }
 
     public static int[][] triangleCreate() {
-        int[][] triangle = new int[11][];
+        int[][] triangle = new int[10][];
 
         for (int i = 0; i < triangle.length; i++) {
 
             triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][triangle[i].length - 1] = 1;
 
         }
 
         for (int i = 0; i < triangle.length; i++) {
 
-            triangle[i][0] = 1;
-
             for (int j = 0; j < triangle[i].length; j++) {
-
-                triangle[i][triangle[i].length - 1] = 1;
-
-                if(i >= 2 && i <= triangle[i].length)
-                    triangle[i][1] = i;
-
-                if(i >= 3 && i <= triangle[i].length)
-                    triangle[i][triangle[i].length - 2] = i;
 
                 if(triangle[i][j] == 0)
                     triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
